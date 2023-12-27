63ba481dd9ea42ff0f8495c8fe69a192fd8837bd
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-12-27 20:40:48 +0200

    form

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask 
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+def home():
+    return render_template('index.html', message='Hello, Flask!')
 
-@app.route('/hello')
-def hello():
-    return 'Hello'
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form['name']
+    return f'Hello, {name}!'
+
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application 
+    # on the local development server.
+    app.run(debug=True)
